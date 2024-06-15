fe3e1b34064684add262525e276295f41eb810e4
Abdurrahman <dreydare16@gmail.com>
2024-06-15 14:59:33 +0100

    completed requirements

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hello!');
+// ELEMENTS SELECTION
+const helpButton = document.querySelector(".help-button");
+const featuresButton = document.querySelector(".features-button");
+const signupButton = document.querySelector(".signup-button");
+
+const help = document.querySelector(".help");
+const features = document.querySelector(".features");
+const signupCTA = document.querySelector(".cta");
+
+// Scroll to section
+const sectionScroll = function (section) {
+  section.scrollIntoView({ behavior: "smooth" });
+};
+
+const scrollEvent = function () {
+  helpButton.addEventListener("click", function () {
+    sectionScroll(help);
+  });
+
+  featuresButton.addEventListener("click", function () {
+    sectionScroll(features);
+  });
+
+  signupButton.addEventListener("click", function () {
+    sectionScroll(signupCTA);
+  });
+};
+scrollEvent();
