64c03e88727e78562b0bc8d263871904fb3da7b8
Abdurrahman <dreydare16@gmail.com>
2024-06-22 09:22:28 +0100

    restructured folder structure
    included storage page and implemented localStorage

---
+++ scripts/storage.js
@@ -0,0 +1,82 @@
+// ELEMENTS SELECTION
+const tableBody = document.querySelector("tbody");
+const form = document.querySelector("form");
+const id = document.querySelector("#id");
+const itemName = document.querySelector("#name");
+const price = document.querySelector("#price");
+const formButton = document.querySelector(".submit");
+let itemData = [];
+
+// FUNCTIONS
+//////////////////////////////////////////////////////////////
+
+// Reset form field
+const reset = function () {
+  id.value = itemName.value = price.value = "";
+};
+
+// Saving to localStorage
+const setLocalStorage = function () {
+  localStorage.setItem("itemData", JSON.stringify(itemData));
+};
+
+const renderData = function () {
+  // Remove existing data
+  tableBody.innerHTML = "";
+
+  //   Format data as html elements
+  const html = itemData
+    .map((item) => {
+      return ` <tr>
+          <td>${item.id}</td>
+          <td>${item.name}</td>
+          <td>${item.price}</td>
+        </tr>`;
+    })
+    .join("");
+
+  // Add data to table
+  tableBody.insertAdjacentHTML("beforeend", html);
+};
+
+// retrieving from localStorage
+const getLocalStorage = function () {
+  const data = JSON.parse(localStorage.getItem("itemData"));
+  if (!data) return;
+
+  itemData = data;
+
+  // Render the data
+  renderData();
+};
+
+const addItem = function (e) {
+  // Prevent browser deafu;s
+  e.preventDefault();
+
+  if (id.value && itemName.value && price.value) {
+    // Add form data to array
+    itemData.push({
+      id: id.value.trim(),
+      name: itemName.value.trim(),
+      price: Number(price.value.trim()),
+    });
+
+    // Show data
+    renderData();
+
+    // Save to localStorage
+    setLocalStorage();
+
+    // Reset form
+    reset();
+  }
+};
+
+// IMPLEMENTATION
+//////////////////////////////////////////////////////
+
+// Call on load
+getLocalStorage();
+formButton.addEventListener("click", (e) => addItem(e));
+form.addEventListener("submit", (e) => addItem(e));
